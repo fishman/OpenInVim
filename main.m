55104d80f746e3622acf5a7f6f1551f5bde5ba06
Reza Jelveh <toyb0mb@gmx.net>
2009-06-09 01:41:50 +0200

    initial commit

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  OpenInVim
+//
+//  Created by Reza Jelveh on 6/9/09.
+//  Copyright Protonet.info 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
